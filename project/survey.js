b733c0ec6ca80beac5499ec26f20e88c0515c5af
Selleo <selleo@Selleos-MacBook-Air.local>
2016-06-23 16:04:42 +0200

    form tags added

---
+++ project/survey.js
@@ -0,0 +1,109 @@
+function Questionnaire(name) {
+  this.name = name;
+  this.questions = [{
+    question: this.question,
+    answers: [],
+    fieldType: this.type
+  }];
+}
+
+var Questionnaires = [];
+
+var nameInput = document.createElement('input');
+document.body.appendChild(nameInput);
+
+var createButton = document.createElement('button');
+createButton.textContent = 'create';
+document.body.appendChild(createButton);
+
+createButton.addEventListener('click', function() {
+    var que = new Questionnaire(nameInput.value);
+
+    var questionInput = document.createElement('input');
+    document.body.appendChild(questionInput);
+
+    var answerInput = document.createElement('input');
+    document.body.appendChild(answerInput);
+
+    var addAnswerButton = document.createElement('button');
+    addAnswerButton.textContent = 'add answer';
+    document.body.appendChild(addAnswerButton);
+
+    var fieldTypeSelect = document.createElement('select');
+
+    var checkbox = document.createElement('option');
+    checkbox.value = 'checkbox'
+    checkbox.textContent = 'checkbox';
+
+    var radio = document.createElement('option');
+    radio.value = 'radio';
+    radio.textContent = 'radio';
+
+    fieldTypeSelect.appendChild(radio)
+    fieldTypeSelect.appendChild(checkbox);
+    document.body.appendChild(fieldTypeSelect);
+
+    var addQuestionButton = document.createElement('button');
+    addQuestionButton.textContent = 'add question';
+    document.body.appendChild(addQuestionButton);
+
+    var answersToAdd = [];
+
+    addAnswerButton.addEventListener('click', function() {
+      answersToAdd.push(answerInput.value);
+      answerInput.value = "";
+    });
+
+    addQuestionButton.addEventListener('click', function() {
+      var choose = fieldTypeSelect.value;
+      que.questions.push({question: questionInput.value, answers: answersToAdd, type: choose});
+      answersToAdd = [];
+      questionInput.value = "";
+      answerInput.value = "";
+    });
+
+    var generate = document.createElement('button');
+    generate.textContent = 'generate';
+    document.body.appendChild(generate);
+
+    generate.addEventListener('click', function() {
+        var form = document.createElement('form');
+        var container = document.createElement('div');
+        document.body.appendChild(container);
+        container.appendChild(form);
+
+        var header = document.createElement('h1');
+        header.textContent = que.name;
+        form.appendChild(header);
+
+        que.questions.forEach(function(question) {
+          var questionHeader = document.createElement('h3');
+          questionHeader.textContent = question.question;
+          form.appendChild(questionHeader);
+          var type = question.type;
+
+          question.answers.forEach(function(answer) {
+            var label = document.createElement('label');
+            label.textContent = answer;
+
+            var answerOption = document.createElement('input');
+            answerOption.type = type;
+            answerOption.name = questionHeader.textContent;
+            answerOption.value = answer;
+
+            label.appendChild(answerOption);
+            form.appendChild(label);
+          });
+        });
+
+        Questionnaires.push(que);
+
+        nameInput.value = "";
+        document.body.removeChild(fieldTypeSelect);
+        document.body.removeChild(questionInput);
+        document.body.removeChild(answerInput);
+        document.body.removeChild(addAnswerButton);
+        document.body.removeChild(addQuestionButton);
+        document.body.removeChild(generate);
+      });
+    });
