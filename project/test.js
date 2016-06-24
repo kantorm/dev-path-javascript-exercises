a6bb41cc2eca28e9faa0eafca2509fccd9c2b1d0
Selleo <selleo@Selleos-MacBook-Air.local>
2016-06-24 13:51:38 +0200

    testing version for removing fields

--- project/test.js
+++ project/test.js
@@ -1,18 +1,127 @@
-//testing space
-
-function Questionnaire(name) {
+function Survey(name) {
   this.name = name;
-  this.questions = [{
-    question: this.question,
-    answers: []
-  }]
+  this.questions = [];
 }
-var example1 = new Questionnaire('example1');
 
-example1.questions.push({question: "to be or", answers: [1,2,3,4]});
-example1.questions.push({question: "not to be", answers: [5,6,7,8]});
+var que = new Survey();
 
-console.log(example1.questions);
-// example1.questions.forEach(function(question){
-//   console.log(question.question + " " + question.answers);
-// })
+var fieldsWraper = document.createElement('div');
+
+function generateFields() {
+    if(fieldsWraper == undefined)
+      var fieldsWraper = document.createElement('div');
+    document.body.appendChild(fieldsWraper);
+
+    var nameInput = document.createElement('input');
+    if(que.name == undefined || que.name == "")
+      nameInput.placeholder = "NAME";
+    else
+        nameInput.value = que.name
+    fieldsWraper.appendChild(nameInput);
+
+    var questionInput = document.createElement('input')
+    questionInput.placeholder = "QUESTION";
+    fieldsWraper.appendChild(questionInput);
+
+    var fieldTypeSelect = document.createElement('select');
+
+    var checkbox = document.createElement('option');
+    checkbox.value = 'checkbox'
+    checkbox.textContent = 'checkbox';
+
+    var radio = document.createElement('option');
+    radio.value = 'radio';
+    radio.textContent = 'radio';
+
+    var input = document.createElement('option');
+    input.value = 'input';
+    input.textContent = 'input';
+
+    fieldTypeSelect.appendChild(radio)
+    fieldTypeSelect.appendChild(checkbox);
+    fieldTypeSelect.appendChild(input);
+    fieldsWraper.appendChild(fieldTypeSelect);
+
+    var answerInput = document.createElement('input');
+    answerInput.placeholder = "ANSWER";
+    fieldsWraper.appendChild(answerInput);
+
+    var answersToAdd = [];
+
+    var addAnswerButton = document.createElement('button');
+    addAnswerButton.textContent = 'add answer';
+    fieldsWraper.appendChild(addAnswerButton);
+
+    var addQuestionButton = document.createElement('button');
+    addQuestionButton.textContent = 'add question';
+    fieldsWraper.appendChild(addQuestionButton);
+
+    addAnswerButton.addEventListener('click', function() {
+      answersToAdd.push(answerInput.value);
+      answerInput.value = "";
+    });
+
+    addQuestionButton.addEventListener('click', function() {
+      if(que.name == undefined) {
+        que.name = nameInput.value;
+      }
+      var choose = fieldTypeSelect.value;
+      que.questions.push({question: questionInput.value, answers: answersToAdd, type: choose});
+
+      answersToAdd = [];
+      answerInput.value = "";
+
+      var newLine = document.createElement('br');
+      document.body.appendChild(newLine);
+
+      console.log(que)
+      generateFields();
+    });
+}
+
+    var generate = document.createElement('button');
+    generate.textContent = 'generate';
+    document.body.appendChild(generate);
+
+    generate.addEventListener('click', function () {
+        var container = document.createElement('div');
+        var form = document.createElement('form');
+
+        document.body.appendChild(container);
+        container.appendChild(form);
+
+        var header = document.createElement('h1');
+        header.textContent = que.name;
+        form.appendChild(header);
+
+        que.questions.forEach(function(question) {
+          var questionHeader = document.createElement('h3');
+          questionHeader.textContent = question.question;
+          form.appendChild(questionHeader);
+          var type = question.type;
+
+          question.answers.forEach(function(answer) {
+            var label = document.createElement('label');
+            label.textContent = answer;
+
+            var answerOption = document.createElement('input');
+            answerOption.type = type;
+            answerOption.name = questionHeader.textContent;
+            answerOption.value = answer;
+
+            label.appendChild(answerOption);
+            form.appendChild(label);
+          });
+        });
+        var submitButton = document.createElement('input');
+        submitButton.type = 'submit';
+        submitButton.value = 'submit';
+        form.appendChild(submitButton);
+
+        sessionStorage.setItem(que.name, JSON.stringify(que));
+        que = new Survey();
+        fieldsWraper.remove();
+
+        generateFields()
+});
+generateFields()
