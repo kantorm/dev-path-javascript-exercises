0decbfa2515e3232a3fcd4e96f34bd1318149e6d
Selleo <selleo@Selleos-MacBook-Air.local>
2016-06-27 15:44:16 +0200

    node server with express js

--- project/server.js
+++ project/server.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var app = express();
 var fs = require('fs');
+var bodyParser = require('body-parser');
 
 var survey = ""
 fs.readFile('survey.js', 'utf8', function(error, script) {
@@ -9,17 +10,24 @@ fs.readFile('survey.js', 'utf8', function(error, script) {
   survey += script;
 })
 
+var urlencodedParser = bodyParser.urlencoded({ extended: false })
+
+app.use(express.static('public'));
+
 app.get('/', function (req, res) {
   res.send('<html><head></head><body><script>'+survey+'</script></body></html>');
 })
 
-
+app.post('/results', urlencodedParser, function (req, res) {
+  var response = req.body;
+  res.end(JSON.stringify(response));
+})
 
 var server = app.listen(8081, function () {
 
   var host = server.address().address
   var port = server.address().port
 
-  console.log("Example app listening at http://%s:%s", host, port)
+  console.log("Survey app listening at http://%s:%s", host, port)
 
 })
