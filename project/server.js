9b80870f45748bacf0e3c1583299522fa4c1a4c8
Selleo <selleo@Selleos-MacBook-Air.local>
2016-06-28 10:20:04 +0200

    adding data into mongodb and finding all entries

--- project/server.js
+++ project/server.js
@@ -2,8 +2,12 @@ var express = require('express');
 var app = express();
 var fs = require('fs');
 var bodyParser = require('body-parser');
+var MongoClient = require('mongodb').MongoClient;
+var assert = require('assert');
 
+var uri = "mongodb://localhost:27017/data";
 var survey = ""
+
 fs.readFile('survey.js', 'utf8', function(error, script) {
   if (error)
   throw error
@@ -16,12 +20,40 @@ app.use(express.static('public'));
 
 app.get('/', function (req, res) {
   res.send('<html><head></head><body><script>'+survey+'</script></body></html>');
+  MongoClient.connect(uri, function(error, db) {
+    assert.equal(null, error);
+    findAnswers(db, function functionName() {
+
+    }ction() {
+      db.close();
+    });
+  });
 })
 
-app.post('/results', urlencodedParser, function (req, res) {
+app.post('/', urlencodedParser, function (req, res) {
   var response = req.body;
-  res.end(JSON.stringify(response));
+  res.end(MongoClient.connect(uri, function(error, db) {
+    if (error)
+      return console.dir(error);
+
+      db.collection('answers').insertOne(response, function(err, result) {
+        assert.equal(err, null);
+        console.log("Inserted a document into the answers collection.");
+      });
+  }))
 })
+
+var findAnswers = function(db, callback) {
+  var cursor = db.collection('answers').find();
+  cursor.each(function(err, doc) {
+    assert.equal(err, null);
+    if (doc != null) {
+      console.log(doc);
+    } else {
+      callback();
+    }
+  });
+};
 
 var server = app.listen(8081, function () {
 
