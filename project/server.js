9cd0deabcd703a65cd87db261f8fe3dc4e1fca0d
Selleo <selleo@Selleos-MacBook-Air.local>
2016-06-28 14:13:46 +0200

    surveys collection

--- project/server.js
+++ project/server.js
@@ -4,6 +4,8 @@ var fs = require('fs');
 var bodyParser = require('body-parser');
 var MongoClient = require('mongodb').MongoClient;
 var assert = require('assert');
+var test = require('./test');
+
 
 var uri = "mongodb://localhost:27017/data";
 var survey = ""
@@ -20,28 +22,41 @@ app.use(express.static('public'));
 
 app.get('/', function (req, res) {
   res.send('<html><head></head><body><script>'+survey+'</script></body></html>');
-  MongoClient.connect(uri, function(error, db) {
-    assert.equal(null, error);
-    findAnswers(db, function functionName() {
-
-    }ction() {
-      db.close();
-    });
-  });
-})
+});
 
 app.post('/', urlencodedParser, function (req, res) {
+  res.send('<html><head></head><body><script>'+survey+'</script></body></html>');
+
   var response = req.body;
+  console.log(response);
   res.end(MongoClient.connect(uri, function(error, db) {
     if (error)
       return console.dir(error);
 
       db.collection('answers').insertOne(response, function(err, result) {
         assert.equal(err, null);
-        console.log("Inserted a document into the answers collection.");
+        console.log('Inserted a document into the answers collection.');
+        db.close();
       });
   }))
-})
+});
+
+app.post('/surveys', urlencodedParser, function (req, res) {
+  res.send('<html><head></head><body><script>'+survey+'</script></body></html>');
+
+  var response = req.body;
+  console.log(response);
+  res.end(MongoClient.connect(uri, function(error, db) {
+    if (error)
+      return console.dir(error);
+
+      db.collection('surveys').insertOne(response, function(err, result) {
+        assert.equal(err, null);
+        console.log('Inserted a document into the surveys collection.');
+        db.close();
+      });
+  }))
+});
 
 var findAnswers = function(db, callback) {
   var cursor = db.collection('answers').find();
