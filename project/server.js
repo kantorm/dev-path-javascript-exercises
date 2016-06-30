f53b63eea3bf8a80ebffbf1e7545a6bc72e7e216
Michał Kantor <kantorm96@gmail.com>
2016-06-30 14:19:34 +0200

    find all questions for specific survey

--- project/server.js
+++ project/server.js
@@ -6,15 +6,15 @@ var MongoClient = require('mongodb').MongoClient;
 var assert = require('assert');
 var pug = require('pug');
 
-var uri = "mongodb://localhost:27017/data";
-var survey = "";
+var uri = "mongodb://localhost:12345/data";
+var surveyTemplate = "";
 var answersArray = [];
 var surveysArray = [];
 
 fs.readFile('views/survey.js', 'utf8', function(error, script) {
   if (error)
     throw error
-  survey += script;
+  surveyTemplate += script;
 });
 
 var urlencodedParser = bodyParser.urlencoded({ extended: false })
@@ -23,17 +23,12 @@ app.use(express.static('public'));
 app.set('view engine', 'pug');
 
 app.get('/', function (req, res) {
-  MongoClient.connect(uri, function(error, db){
-    if (error)
-      throw error;
-    findAnswers();
-    res.render('index', {surveyTemplate: survey, answers: answersArray});
-  })
+    res.render('index', {surveyTemplate: surveyTemplate, answers: []});
 });
 
 app.post('/', urlencodedParser, function (req, res) {
   var response = req.body;
-
+  console.log();
   MongoClient.connect(uri, function(error, db) {
   //saving answers in database
     if (error)
@@ -41,30 +36,38 @@ app.post('/', urlencodedParser, function (req, res) {
     db.collection('answers').insertOne(response, function(err, result) {
       assert.equal(err, null);
       console.log('Inserted a document into the answers collection.');
-    });
-    findAnswers(db, response);
-    res.render('index', {surveyTemplate: survey, answers: answersArray});
+
+      res.render('index', {surveyTemplate: surveyTemplate, answers: response});
   })
 });
+});
 
-app.post('/surveys', urlencodedParser, function (req, res) {
-  res.render('index', {surveyTemplate: survey});
-
+app.post(/\w|\s/g , urlencodedParser, function (req, res) {
   var response = req.body;
-
+  var response = JSON.parse(response.survey);
+  console.log(response.questions);
   //Adding survey into databse
-  res.send(MongoClient.connect(uri, function(error, db) {
+  MongoClient.connect(uri, function(error, db) {
     if (error)
       return console.log(error);
     db.collection('surveys').insertOne(response, function(err, result) {
       assert.equal(err, null);
       console.log('Inserted a document into the surveys collection.');
-
-      db.close();
     });
-  }))
-  res.end()
+  });
+  res.render('index', {surveyTemplate: surveyTemplate, answers: []});
 });
+
+//queries to database
+var findQuestionFromSurvey = function(db, response) {
+  var cursor = db.collection('surveys').find({name: response.survey})
+    cursor.each(function(err, doc) {
+      assert.equal(err, null);
+      if (doc != null) {
+        console.log(doc.questions);
+    }
+  });
+};
 
 var findAnswers = function(db, response) {
   var array = [];
@@ -78,7 +81,8 @@ var findAnswers = function(db, response) {
     }
   });
 };
-var findSurveys = function(db, callback) {
+
+var findSurveys = function(db) {
   var array = [];
   var cursor = db.collection('surveys').find();
   cursor.each(function(err, doc) {
