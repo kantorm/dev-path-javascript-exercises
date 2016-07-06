a49b4204cb72023dba36bec21106fd63cb072cb1
kantorm <kantorm@gmail.com>
2016-07-06 09:33:20 +0200

    saving surveys from db in session storage

--- project/views/script.js
+++ project/views/script.js
@@ -1,74 +1,69 @@
-        var surveyToGenerate = new Object();
-        surveyToGenerate.questions = [];
+var surveyToGenerate = new Object();
+surveyToGenerate.questions = [];
 
-        var answers = [];
-        $('#addAnswer').click(function() {
-          var toAdd = $('#answerField').val();
-          answers.push(toAdd);
-          $('#answerField').val('').focus();
-        });
+var answers = [];
+$('#addAnswer').click(function() {
+  var toAdd = $('#answerField').val();
+  answers.push(toAdd);
+  $('#answerField').val('').focus();
+});
 
-        $('#addQuestion').click(function() {
-            if (surveyToGenerate.name == undefined)
-              surveyToGenerate.name = $('#nameField').val();
+$('#addQuestion').click(function() {
+  if (surveyToGenerate.name == undefined)
+    surveyToGenerate.name = $('#nameField').val();
 
-            var fieldType = $('#fieldType').val();
-            surveyToGenerate.questions.push({
-              questionName: $('#questionField').val(),
-              answers: answers,
-              fieldType: fieldType
-              });
+  var fieldType = $('#fieldType').val();
+  surveyToGenerate.questions.push({
+      questionName: $('#questionField').val(),
+      answers: answers,
+      fieldType: fieldType
+  });
 
-            answers = [];
-            $('#questionField').val('').focus();
-            $('#answerField').val('');
-        });
+  answers = [];
+  $('#questionField').val('').focus();
+  $('#answerField').val('');
+});
 
-        $('#surveyFields').submit(function(event) {
+$('#surveyFields').submit(function(event) {
 
-          var data = JSON.stringify(surveyToGenerate)
-          $.post('/surveys', data)
+  var data = JSON.stringify(surveyToGenerate)
+  $.post('/surveys', data)
 
-          sessionStorage.setItem(surveyToGenerate.name, data);
-          generateSurvey(surveyToGenerate);
+  generateSurvey(surveyToGenerate);
+  event.preventDefault();
+});
 
-          event.preventDefault();
-          });
+function generateSurvey(toGenerate) {
 
-        var generateSurvey = function(toGenerate) {
+  var formId = toGenerate.name.replace(/\s/g , '_');
+  $('#generated').append($('<form id='+formId+'>'));
+  formId = '#'+formId;
+  $(formId).attr({action: '/', method: 'POST'});
+  $(formId).append('<h1>'+toGenerate.name+'</h1>');
+  $(formId).append('<input style="display: none" value="'+toGenerate.name+'" name="surveyName">')
 
-          var formId = toGenerate.name.replace(/\s/g , '_');
-          $('#generated').append($('<form id='+formId+'>'));
-          formId = '#'+formId;
-          $(formId).attr({action: '/', method: 'POST'});
-          $(formId).append('<h1>'+toGenerate.name+'</h1>');
-          $(formId).append('<input style="display: none" value="'+toGenerate.name+'" name="surveyName">')
+  toGenerate.questions.forEach(function(question){
+    $(formId).append('<h3>'+question.questionName+'</h3>')
 
-          toGenerate.questions.forEach(function(question){
-            $(formId).append('<h3>'+question.questionName+'</h3>')
-
-              question.answers.forEach(function(answer) {
-                var input = '<label><input name="'+question.questionName+
+    question.answers.forEach(function(answer) {
+      var input = '<label><input name="'+question.questionName+
                 '" type="'+question.fieldType+'" value="'+answer+'">'+answer+'</label>';
-                $(formId).append($(input));
-                });
-              });
-              var submit = '<input type = "submit"/>'
-              $(formId).append(submit);
+      $(formId).append($(input));
+    });
+  });
+  var submit = '<input type = "submit"/>'
+  $(formId).append(submit);
 
-              $('#nameField').val('').focus();
+  $('#nameField').val('').focus();
 
-              surveyToGenerate = {};
-              surveyToGenerate.questions = [];
-          }
-          function generateFormStorage() {
-            for (var survey in sessionStorage) {
-              var parsed = JSON.parse(sessionStorage[survey]);
-              generateSurvey(parsed);
-            }
-          }
-          function sampleFunc(arg) {
-            console.log(arg);
-          }
+  surveyToGenerate = {};
+  surveyToGenerate.questions = [];
+}
+function generateFormStorage() {
+  for (var survey in sessionStorage) {
+    var parsed = JSON.parse(sessionStorage[survey]);
+    generateSurvey(parsed);
+  }
+}
 
-        $(document).ready(generateFormStorage())
+$(document).ready(generateFormStorage())
