23bd4d4e6411384c8ea3e2d3cd69e1d67929c089
kantorm <kantorm@gmail.com>
2016-07-06 15:19:06 +0200

    first ES6 elements

--- project/views/script.js
+++ project/views/script.js
@@ -1,9 +1,9 @@
-var surveyToGenerate = new Object();
+let surveyToGenerate = new Object();
 surveyToGenerate.questions = [];
 
-var answers = [];
+let answers = [];
 $('#addAnswer').click(function() {
-  var toAdd = $('#answerField').val();
+  let toAdd = $('#answerField').val();
   answers.push(toAdd);
   $('#answerField').val('').focus();
 });
@@ -12,7 +12,7 @@ $('#addQuestion').click(function() {
   if (surveyToGenerate.name == undefined)
     surveyToGenerate.name = $('#nameField').val();
 
-  var fieldType = $('#fieldType').val();
+  let fieldType = $('#fieldType').val();
   surveyToGenerate.questions.push({
       questionName: $('#questionField').val(),
       answers: answers,
@@ -26,7 +26,7 @@ $('#addQuestion').click(function() {
 
 $('#surveyFields').submit(function(event) {
 
-  var data = JSON.stringify(surveyToGenerate)
+  let data = JSON.stringify(surveyToGenerate)
   $.post('/surveys', data)
 
   generateSurvey(surveyToGenerate);
@@ -35,23 +35,23 @@ $('#surveyFields').submit(function(event) {
 
 function generateSurvey(toGenerate) {
 
-  var formId = toGenerate.name.replace(/\s/g , '_');
+  let formId = toGenerate.name.replace(/\s/g , '_');
   $('#generated').append($('<form id='+formId+'>'));
   formId = '#'+formId;
   $(formId).attr({action: '/', method: 'POST'});
-  $(formId).append('<h1>'+toGenerate.name+'</h1>');
-  $(formId).append('<input style="display: none" value="'+toGenerate.name+'" name="surveyName">')
+  $(formId).append(`<h1>${toGenerate.name}</h1>`);
+  $(formId).append(`<input style="display: none" value="${toGenerate.name}" name="surveyName">`)
 
-  toGenerate.questions.forEach(function(question){
-    $(formId).append('<h3>'+question.questionName+'</h3>')
+  for (let question of toGenerate.questions) {
+    $(formId).append(`<h3>${question.questionName}</h3>`)
 
-    question.answers.forEach(function(answer) {
-      var input = '<label><input name="'+question.questionName+
-                '" type="'+question.fieldType+'" value="'+answer+'">'+answer+'</label>';
+    for (let answer of question.answers) {
+      let input = `<label><input name="${question.questionName}
+                " type="${question.fieldType}" value="${answer}">${answer}</label>`;
       $(formId).append($(input));
-    });
-  });
-  var submit = '<input type = "submit"/>'
+    }
+  }
+  let submit = '<input type = "submit"/>'
   $(formId).append(submit);
 
   $('#nameField').val('').focus();
