269f657d2e8d442ba791ff66f8eb09f6e4d3b725
kantorm <kantorm@gmail.com>
2016-07-14 14:05:54 +0200

    key attribute for form fields

--- react-project/views/generateFromDb.jsx
+++ react-project/views/generateFromDb.jsx
@@ -4,9 +4,11 @@ var GenerateFromDb = React.createClass({
       <div>{surveysArray.map(function(survey) {
         var action = `/surveys/${survey.surveyName.replace(/ /g,'-')}/results`;
 
-          return <form action={action} method='POST' name={survey.surveyName}><h1>{survey.surveyName}</h1> {survey.questions.map(function(question){
-            return <div><h3>{question.questionName}</h3> {question.answers.map(function(answerOption){
-              return <label><input type={question.fieldType} value={answerOption} name={question.questionName}/>
+          return <form key={survey._id}action={action} method='POST' name={survey.surveyName}>
+                    <h1>{survey.surveyName}</h1> {survey.questions.map(function(question){
+            return    <div key={survey._id+question.questionName}>
+                        <h3>{question.questionName}</h3> {question.answers.map(function(answerOption){
+              return      <label key={survey._id+answerOption}><input name={question.questionName} type={question.fieldType} value={answerOption} />
                   {answerOption}</label>
             })}</div>
         })}<input defaultValue={survey.surveyName} name='surveyName' style={{display: 'none'}}/>
